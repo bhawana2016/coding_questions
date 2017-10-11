9818e253b7943521ecd24a887e72d7ba1daa5efd
Bhawana Gupta <bhawanagupta15@gmail.com>
2017-10-11 07:12:29 +0530

    initial commit

---
+++ topcoder_AlphabatOrderDiv2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+string f(vector<string> v){
+    vector< set<char> > parents(26);
+    int n=v.size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<v[i].size()-1;j++){
+            char x=v[i][j];
+            char y=v[i][j+1];
+            if(x==y) continue;
+            if(parents[x-'a'].count(y)>0) return "Impossible";
+            parents[y-'a'].insert(x);
+            if(parents[x-'a'].size()!=0){
+                parents[y-'a'].insert(parents[x-'a'].begin(),parents[x-'a'].end());
+            }
+        }
+    }
+    return "possible";
+}
+int main(){
+    cout<< " enter strings"<<"\n";
+    string s;
+    vector<string> v;
+    while(cin>>s){
+        v.push_back(s);
+    }
+    cout<<f(v);
+}
